001e26b6215b5899a2fba7962081d68eeeb48fb7
sharon smith <sharon.e.smith2@gmail.com>
2017-02-20 00:30:41 -0600

    setting up interaction between index.html and main.js

---
+++ javascripts/main.js
@@ -0,0 +1,25 @@
+
+
+
+function getBread (sandwichArray) {
+
+	var breadChoices = document.getElementByName("chk_bread");
+
+	for (var i=0; i<breadChoices.length; i++) {
+  		if (breadChoices[i].checked) {
+  			// add bread to sandwich ; push choices onto array
+    	}
+	}
+}
+
+
+
+var buildButton = document.getElementById("buildSandwich");
+
+buildButton.addEventListener("click", getIngredients);
+
+function getIngredients(){
+	var sandwich = [];
+
+	getBread(sandwich);
+}
